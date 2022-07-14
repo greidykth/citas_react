c005b12e0dcef771af1721ffb7e36052900aba95
Greidy Peña <greidykp@gmail.com>
2022-07-14 16:04:11 -0500

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+const Error = ({children}) => {
+  return (
+    <div className="bg-red-700 rounded-sm font-bold text-white uppercase text-center p-2 mb-2">
+        {children}
+    </div>
+  )
+}
+
+export default Error;    
